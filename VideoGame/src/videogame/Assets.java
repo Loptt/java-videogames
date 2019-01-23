f9cc21cbb7e77eb4d72f5d927616c4c43a721921
Carlos Estrada <carlos.deg02@hotmail.com>
2019-01-23 10:22:28 -0600

    Added first videogame

---
+++ VideoGame/src/videogame/Assets.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videogame;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author charles
+ */
+public class Assets {
+
+    public static BufferedImage background;
+    public static BufferedImage player;
+    
+    public static void init() {
+        background = ImageLoader.loadImage("/images/background_mario.jpg");
+        player = ImageLoader.loadImage("/images/mario.png");
+    }
+}
